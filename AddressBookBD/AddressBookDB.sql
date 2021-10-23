e07a68d9c8c897845f0753bf9bf61d41f26fef16
chaitanya4294 <chaitanya.4294@gmail.com>
2021-10-23 08:01:32 +0530

    [Chaitanya] Add : Added UC2 to create table address_book

--- AddressBookBD/AddressBookDB.sql
+++ AddressBookBD/AddressBookDB.sql
@@ -4,7 +4,19 @@ CREATE DATABASE AddressBookService
 
 USE AddressBookService
 
--- UC2 =>
+-- UC2 => Create table AddressBook
+CREATE TABLE address_book
+(
+FirstName VARCHAR(50) not null,
+LastName VARCHAR(50) not null,
+Address VARCHAR(100) not null,
+City VARCHAR(50) not null,
+State VARCHAR(50) not null,
+Zipcode VARCHAR(6) not null,
+PhoneNumber VARCHAR(10) not null,
+Email VARCHAR(50) not null
+)
+SELECT * FROM address_book
 
 -- UC3 =>
 
